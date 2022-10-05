69201b5674bce11bfaa72ac066b9e6a44cdf5e46
Ivan <ivanpronin1379@gmail.com>
2022-10-05 23:16:48 +0500

    fixed typescript and started setting up endpoints

--- dist/index.js
+++ dist/index.js
@@ -5,13 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('%database%');
+const port = 5000;
+app.get('/api/gettasks', (req, res) => {
+    res.json({ sth: 'sth' });
 });
-app.post('/api/login', (req, res) => {
-    res.send('login info got');
+app.post('/api/createtask', (req, res) => {
+    res.json('successfully created');
+});
+app.post('/api/deletetask', (req, res) => {
+    res.json('successfully deleted');
 });
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
-app.get('/', (req, res) => {
-	res.send('%database%');
+app.get('/api/gettasks', (req, res) => {
+	res.json({ sth: 'sth' });
 });
 
-app.post('/api/login', (req, res) => {
-	res.send('login info got');
+app.post('/api/createtask', (req, res) => {
+	res.json('successfully created');
+});
+
+app.post('/api/deletetask', (req, res) => {
+	res.json('successfully deleted');
 });
 
 app.listen(port, () => {
