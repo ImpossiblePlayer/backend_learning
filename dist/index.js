2698ee8912c8277139bdaebeaf3c9328ce50d6ac
Ivan <ivanpronin1379@gmail.com>
2022-10-09 21:51:42 +0500

    set up few endpoints

--- dist/index.js
+++ dist/index.js
@@ -6,14 +6,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 5000;
-app.get('/api/gettasks', (req, res) => {
-    res.json({ sth: 'sth' });
+const jsonBodyMiddleware = express_1.default.json();
+app.use(jsonBodyMiddleware);
+const db = {
+    vacancies: [
+        { id: 1, title: 'senior front-end developer' },
+        { id: 2, title: 'middle back-end developer' },
+    ],
+};
+app.get('/api/vacancies', (req, res) => {
+    if (!req.body.title)
+        return db;
+    const filteredTitles = db.vacancies.filter((el) => el.title.indexOf(req.body.title) > -1);
+    filteredTitles ? res.json(filteredTitles) : res.sendStatus(404);
+    res.json(db.vacancies);
 });
-app.post('/api/createtask', (req, res) => {
-    res.json('successfully created');
+app.post('/api/vacancies', (req, res) => {
+    db.vacancies.push({ id: +new Date(), title: req.body.title });
+    res.sendStatus(201);
 });
-app.post('/api/deletetask', (req, res) => {
-    res.json('successfully deleted');
+app.delete('/api/vacancies/:id', (req, res) => {
+    if (!db.vacancies.find((el) => el.id === +req.params.id)) {
+        res.sendStatus(404);
+        return;
+    }
+    delete db.vacancies[+req.params.id];
+    res.sendStatus(200);
 });
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,41 @@ import express from 'express';
 const app = express();
 const port = 5000;
 
-app.get('/api/gettasks', (req, res) => {
-	res.json({ sth: 'sth' });
+const jsonBodyMiddleware = express.json();
+app.use(jsonBodyMiddleware);
+
+const db = {
+	vacancies: [
+		{ id: 1, title: 'senior front-end developer' },
+		{ id: 2, title: 'middle back-end developer' },
+	],
+};
+
+app.get('/api/vacancies', (req, res) => {
+	if (!req.body.title) return db;
+
+	const filteredTitles = db.vacancies.filter(
+		(el) => el.title.indexOf(req.body.title as string) > -1
+	);
+
+	// если такая запись есть, то возвращает ее, иначе - 404
+	filteredTitles ? res.json(filteredTitles) : res.sendStatus(404);
+	res.json(db.vacancies);
 });
 
-app.post('/api/createtask', (req, res) => {
-	res.json('successfully created');
+app.post('/api/vacancies', (req, res) => {
+	db.vacancies.push({ id: +new Date(), title: req.body.title });
+	res.sendStatus(201);
 });
 
-app.post('/api/deletetask', (req, res) => {
-	res.json('successfully deleted');
+app.delete('/api/vacancies/:id', (req, res) => {
+	if (!db.vacancies.find((el) => el.id === +req.params.id)) {
+		// если
+		res.sendStatus(404);
+		return;
+	}
+	delete db.vacancies[+req.params.id];
+	res.sendStatus(200);
 });
 
 app.listen(port, () => {
